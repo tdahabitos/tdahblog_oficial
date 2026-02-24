2b08a739d27c367ffba3ec452650086d19fe9203
tdahabitos <haiderbcriziva@gmail.com>
2026-02-24 17:23:10 -0300

    chore: remove _keep_blog snapshot

--- _keep_blog/src/consts.ts
+++
@@ -1,5 +0,0 @@
-export const SITE_TITLE = 'TDAH Blog'
-export const SITE_DESCRIPTION = 'Tudo sobre o mundo TDAH'
-export const SITE_IMAGE_PLACEHOLDER = '/og-placeholder.jpg'
-export const API_URL = import.meta.env.API_URL
-export const STORAGE_URL = import.meta.env.STORAGE_URL
